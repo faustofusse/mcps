7988ab33e40d0cfad08b26aae77c7f9360de025a
Fausto Fusse <faustofusse@gmail.com>
2025-08-20 09:03:21 -0300

    feat(browser): screenshot tool

--- browser/go.mod
+++ browser/go.mod
@@ -3,6 +3,14 @@ module mcp-browser
 go 1.24.4
 
 require (
+	github.com/chromedp/cdproto v0.0.0-20250803210736-d308e07a266d // indirect
+	github.com/chromedp/chromedp v0.14.1 // indirect
+	github.com/chromedp/sysutil v1.1.0 // indirect
+	github.com/go-json-experiment/json v0.0.0-20250813233538-9b1f9ea2e11b // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
 	github.com/modelcontextprotocol/go-sdk v0.2.0 // indirect
 	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
+	golang.org/x/sys v0.35.0 // indirect
 )
--- browser/main.go
+++ browser/main.go
@@ -3,43 +3,57 @@ package main
 import (
 	"context"
 
+	"github.com/chromedp/chromedp"
 	"github.com/modelcontextprotocol/go-sdk/mcp"
 )
 
-// func SayHello(ctx context.Context, req *mcp.ServerR)
-
 func main() {
-	// ctx := context.Background()
+	ctx := context.Background()
 
-	implementation := &mcp.Implementation{ Name: "mcp-compose", Version: "v0.0.1" }
+	implementation := &mcp.Implementation{ Name: "mcp-browser", Version: "v0.0.1" }
 
 	server := mcp.NewServer(implementation, &mcp.ServerOptions{})
 
-	type HelloParams struct {
-		Name string `json:"name" jsonschema:"the name of the person to greet"`
-	}
-	// type HelloResult struct {
-	// 	Text string `json:"name" jsonschema:"greet text"`
-	// }
-	mcp.AddTool(server, &mcp.Tool{
-		Name: "greet",
-		Description: "say hello",
-	}, func(ctx context.Context, ss *mcp.ServerSession, params *mcp.CallToolParamsFor[HelloParams]) (*mcp.CallToolResultFor[struct{}], error) {
-		return &mcp.CallToolResultFor[struct{}]{
-			Content: []mcp.Content{
-				&mcp.TextContent{
-					Text: "Hello " + params.Name,
-				},
-			},
-		}, nil
-	})
+	mcp.AddTool(server, &ScreenshotTool, Screenshot)
 
 	// handler := func(r *http.Request) *mcp.Server { return server }
 	// http.ListenAndServe(":8080", mcp.NewStreamableHTTPHandler(handler, nil))
 
-	ctx := context.Background()
-	transport := mcp.NewStdioTransport()
-	// transport := mcp.NewLoggingTransport(&mcp.StdioTransport{}, os.Stderr)
-	err := server.Run(ctx, transport)
+	err := server.Run(ctx, mcp.NewStdioTransport())
 	if err != nil { panic(err) }
+}
+
+type ScreenshotParams struct {
+	Url string `json:"url" jsonschema:"url of the webpage"`
+}
+
+var ScreenshotTool = mcp.Tool{
+	Name: "screenshot",
+	Description: "take screenshot of webpage",
+}
+
+type ScreenshotResult struct {}
+
+func Screenshot(ctx context.Context, ss *mcp.ServerSession, params *mcp.CallToolParamsFor[ScreenshotParams]) (*mcp.CallToolResultFor[ScreenshotResult], error) {
+	ctx, cancel := chromedp.NewContext(ctx)
+	defer cancel()
+
+	var image []byte
+
+	err := chromedp.Run(ctx, chromedp.Tasks{
+		chromedp.Navigate(params.Arguments.Url),
+		chromedp.FullScreenshot(&image, 90),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &mcp.CallToolResultFor[ScreenshotResult]{
+		Content: []mcp.Content{
+			&mcp.ImageContent{
+				Data: image,
+				MIMEType: "image/jpeg",
+			},
+		},
+	}, nil
 }
